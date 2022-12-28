2928369d7c9f747e7f5f8fe3205400f167e64966
meghana121198 <109966559+meghana121198@users.noreply.github.com>
2022-12-28 23:39:17 +0530

    Update more1.jsx

--- src/components/more1.jsx
+++ src/components/more1.jsx
@@ -12,9 +12,9 @@ return (
     <div className='d-flex align-items-center'>
         <div className='picl11 pdd1'></div>
         <div className='pdd1'>
-            <h6>Flowers gives birth</h6>
+            <h6>root of the environmental crisis.</h6>
             <span className='font-weight-light' style={{fontSize:"10px"}}>
-            Open your senses to the natural wonders around you and let yourself.
+            The absence of a deep emotional connection between humans and the natural world
             </span><br /><br /><br />
             <footer className='blockquote-footer mb-0 mr-0 fs-7'>Megha gowda@yil</footer>
         </div>
