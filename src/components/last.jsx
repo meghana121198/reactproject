6ad0d469972fe08d244634f63978790f216211a9
meghana121198 <109966559+meghana121198@users.noreply.github.com>
2022-12-28 23:27:49 +0530

    Update last.jsx

--- src/components/last.jsx
+++ src/components/last.jsx
@@ -5,9 +5,11 @@ export default function Last() {
 return (
     <>
     <div className='lstt'>
-    <h4>If you like Follow your dreams dont ever hesitate to start</h4><br/>
-    Open your senses to the natural wonders around you and let yourself become immersed in the superb beauty and splendor of flowers, and the wonders of Scared Geometry.
-    <br/><br/><br/>Travel / <font style={{color:"#0202"}}> I like these in 2020</font>
+    <h4>Trying and searching and wanting</h4><br/>
+    I am sitting on my balcony. It is spring and there is a little bit of heat in the sun. The balcony looks out over a road. The road is usually busy… an endless stream of trucks and cars but right now there is no traffic. Everyone is self-isolating. The machine has stopped. It feels…
+
+
+    <br/><br/><br/>Travel / <font style={{color:"#0202"}}>Environment, Mindfulness, Nature</font>
     </div>
     </>
 )
