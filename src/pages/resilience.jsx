410949bb04bbd361a37840800f816ca0fbc9aabd
meghana121198 <109966559+meghana121198@users.noreply.github.com>
2022-12-28 23:09:25 +0530

    Update and rename hills.jsx to resilience.jsx

--- src/pages/resilience.jsx
+++ src/pages/resilience.jsx
@@ -3,12 +3,12 @@ import Header from '../components/header'
 import '../app.css'
 import Xxx from '../components/xxx'
 
-export default function Hills() {
+export default function resilience() {
   return (
     <>
     <Header />
     <br />
-    <h1>Hills</h1>
+    <h1>resilience</h1>
     <div className='blne'></div> <br />
     <Xxx/>
     
