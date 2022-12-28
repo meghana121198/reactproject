29d79309e364e23607bff8b9db99da7cf6d0a79a
meghana121198 <109966559+meghana121198@users.noreply.github.com>
2022-12-28 23:15:34 +0530

    Update and rename tribes.jsx to sustainable.jsx

--- src/pages/sustainable.jsx
+++ src/pages/sustainable.jsx
@@ -3,12 +3,12 @@ import Header from '../components/header'
 import '../app.css'
 import Xxx from '../components/xxx'
 
-export default function Tribes() {
+export default function sustainable() {
   return (
     <>
     <Header />
     <br />
-    <h1>Tribes</h1>
+    <h1>sustainable</h1>
     <div className='blne'></div> <br />
     <Xxx/>
     </>
